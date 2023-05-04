4fc8842ced788a092679117ddb7d7bb0cec57c21
Victor Luck <viktor_carvvalho@hotmail.com>
2023-05-04 00:11:17 -0300

    criação da implementation

---
+++ 03_restwithaspnet/restUdemy/restUdemy/Services/IPersonService.cs
@@ -0,0 +1,15 @@
+﻿using restUdemy.Models;
+
+namespace restUdemy.Services
+{
+    public interface IPersonService
+    {
+        Person Create(Person person);
+        Person Update(Person person);
+        void Delete(Person person);
+        Person Get(long id);
+        List<Person> GetAll();
+
+
+    }
+}
---
+++ 03_restwithaspnet/restUdemy/restUdemy/Services/Implementations/PersonServiceImplementation.cs
@@ -0,0 +1,38 @@
+﻿using restUdemy.Models;
+
+namespace restUdemy.Services.Implementations
+{
+    public class PersonServiceImplementation : IPersonService
+    {
+        public Person Create(Person person)
+        {
+            return person;
+        }
+
+        public void Delete(Person person)
+        {
+            
+        }
+
+        public Person Get(long id)
+        {
+            return new Person { 
+                Id        = id ,
+                FirstName = "Victor",
+                LastName  = "Luck",
+                Address   = "Rua 1",
+                Gender    = "Male"
+            };
+        }
+
+        public List<Person> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Person Update(Person person)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
